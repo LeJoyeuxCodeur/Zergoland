f9c2801d01d94d2a3340d7932b1349377b73fb74
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2014-06-13 21:12:12 +0200

    update

---
+++ src/Constante.java
@@ -0,0 +1,22 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Constante {
+	public static final Image LOGO = new ImageIcon("ressources/images/logo/logo.png").getImage();
+	public static final int CASES_X = 15;
+	public static final int CASES_Y = 25;
+	public static final ImageIcon caseVide = new ImageIcon("ressources/images/cases/CaseVide.png");
+	public static final ImageIcon casePerso = new ImageIcon("ressources/images/cases/CasePerso.png");
+	public static final ImageIcon caseArbre = new ImageIcon("ressources/images/cases/CaseArbre.png");
+	public static final ImageIcon maisonG = new ImageIcon("ressources/images/cases/maisonG.png");
+	public static final ImageIcon maisonM = new ImageIcon("ressources/images/cases/maisonM.png");
+	public static final ImageIcon maisonD = new ImageIcon("ressources/images/cases/maisonD.png");
+	public static final ImageIcon coffre = new ImageIcon("ressources/images/cases/coffre.png");
+	public static final int HP_BASE = 100;
+	public static final int MP_BASE = 100;
+	public static final int XP_BASE = 0;
+	public static final int OR_BASE = 0;
+	public static final int LVL_BASE = 1;
+	public static final String MAP = "ressources/mapsPattern/mapTest2.txt";
+}
---
+++ src/MenuPrincipal.java
@@ -0,0 +1,12 @@
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+
+public class MenuPrincipal extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public MenuPrincipal() {
+		this.setBounds(new Rectangle(1100, 600));
+	}
+}
