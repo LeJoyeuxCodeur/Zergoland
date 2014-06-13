fe1518dbd0296f05bdb8422e73fd348449e9b841
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2014-06-13 15:25:33 +0200

    update base

--- src/FenetreDeJeu.java
+++ src/FenetreDeJeu.java
@@ -1,17 +1,22 @@
 import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class FenetreDeJeu extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private Case[][] map;
-	private JPanel[][] panels;
-	
+	private JLabel[][] panels;
+	private BufferedReader reader;
+	private int[][] pattern;
+
 	public FenetreDeJeu() {
 		super("ZergoLand");
-		setLayout(new GridLayout(Constante.CASES_X, Constante.CASES_Y));
+		setLayout(new GridLayout(Constante.CASES_X, Constante.CASES_Y, -5, 0));
+		initReader();
 		initMap();
 		initFenetre();
 	}
@@ -22,15 +27,35 @@ public class FenetreDeJeu extends JFrame {
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	public void initReader() {
+		String[] patternTmp = null;
+		int cpt = 0;
+		try {
+			pattern = new int[Constante.CASES_X][Constante.CASES_Y];
+			reader = new BufferedReader(new FileReader("ressources/mapsPattern/mapTest.txt"));
+			while (reader.ready()) {
+				patternTmp = reader.readLine().split(",");
+				for (int i = 0; i < patternTmp.length; i++)
+					pattern[cpt][i] = Integer.parseInt(patternTmp[i]);
+				cpt++;
+			}
+		}
+		catch (IOException e) {}
+	}
 	public void initMap() {
 		map = new Case[Constante.CASES_X][Constante.CASES_Y];
-		panels = new JPanel[Constante.CASES_X][Constante.CASES_Y];
+		panels = new JLabel[Constante.CASES_X][Constante.CASES_Y];
 
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
-				map[i][j] = new Case(Constante.caseArbre);
-				panels[i][j] = new JPanel();
+				if (pattern[i][j] == 0)
+					map[i][j] = new Case(Constante.caseVide);
+				else if (pattern[i][j] == 1)
+					map[i][j] = new Case(Constante.caseArbre);
+
+				panels[i][j] = new JLabel();
 				panels[i][j].add(new JLabel(map[i][j].getSkin()));
+				panels[i][j].setIcon(map[i][j].getSkin());
 				add(panels[i][j]);
 			}
 		}
