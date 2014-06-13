b9ec5ece657e9b8f44498063b7477ca9fd52145b
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2014-06-13 22:37:43 +0200

    update

--- src/FenetreDeJeu.java
+++ src/FenetreDeJeu.java
@@ -32,7 +32,7 @@ public class FenetreDeJeu extends JFrame {
 		addKeyListener(ecouteurDepl);
 	}
 	private void initFenetre() {
-		setSize(1280, 600);
+		setSize(1280, 700);
 		setResizable(false);
 		setIconImage(Constante.LOGO);
 		setLocationRelativeTo(null);
