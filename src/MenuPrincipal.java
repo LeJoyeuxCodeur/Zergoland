529ffed063fffee6dd00e95200b9b43b5fe4b560
bloodyraven <bloody.duj@gmail.com>
2014-06-13 23:07:03 +0200

    up

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,12 +1,41 @@
-import java.awt.Rectangle;
-
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MenuPrincipal extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JButton nouveau = new JButton("Nouveau Jeu"), continuer = new JButton("Continuer"), exit = new JButton("Quitter");
 
 	public MenuPrincipal() {
-		this.setBounds(new Rectangle(1100, 600));
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		nouveau.setAlignmentX(Component.CENTER_ALIGNMENT);
+		continuer.setAlignmentX(Component.CENTER_ALIGNMENT);
+		exit.setAlignmentX(Component.CENTER_ALIGNMENT);
+		ButtonListener al = new ButtonListener();
+		nouveau.addActionListener(al);
+		continuer.addActionListener(al);
+		exit.addActionListener(al);
+		add(nouveau);
+		add(continuer);
+		add(exit);
+		
+	}
+	private class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==nouveau) {
+				
+			}
+			if(e.getSource()==continuer) {
+				
+			}
+			if(e.getSource()==exit) {
+				System.exit(0);
+			}
+		}	
 	}
 }
---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class Test {
+	
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setContentPane(new MenuPrincipal());
+		f.setSize(1200, 700);
+		f.setVisible(true);
+	}
+}
