d6d1098158fe0792dbb0b86964a3cffc79e62a14
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2014-06-14 19:59:31 +0200

    update

--- src/Constante.java
+++ src/Constante.java
@@ -6,21 +6,32 @@ public class Constante {
 	public static final Image LOGO = new ImageIcon("ressources/images/logo/logo.png").getImage();
 	public static final int CASES_X = 15;
 	public static final int CASES_Y = 22;
-	public static final ImageIcon caseVide = new ImageIcon("ressources/images/cases/CaseVide.png");
-	public static final ImageIcon casePerso = new ImageIcon("ressources/images/cases/Bryan2.png");
-	public static final ImageIcon caseArbre = new ImageIcon("ressources/images/cases/CaseArbre.png");
-	public static final ImageIcon maisonHG = new ImageIcon("ressources/images/cases/MaisonHG.png");
-	public static final ImageIcon maisonHD = new ImageIcon("ressources/images/cases/MaisonHD.png");
-	public static final ImageIcon maisonBG = new ImageIcon("ressources/images/cases/MaisonBG.png");
-	public static final ImageIcon maisonBD = new ImageIcon("ressources/images/cases/MaisonBD.png");
-	public static final ImageIcon coffre = new ImageIcon("ressources/images/cases/coffre.png");
-	public static final ImageIcon coffreOuvert = new ImageIcon("ressources/images/cases/coffreOuvert.png");
-	public static final ImageIcon barriere = new ImageIcon("ressources/images/cases/barriere.png");
 	public static final int HP_BASE = 100;
 	public static final int MP_BASE = 100;
 	public static final int XP_BASE = 0;
 	public static final int OR_BASE = 0;
 	public static final int LVL_BASE = 1;
-	public static final String MAP = "ressources/mapsPattern/mapTest2.txt";
 	public static final int MP_MAX = 100;
+	
+	// Mode Jeu
+	public static final String MAP_DEPLACEMENT = "ressources/mapsPattern/mapTest2.txt";
+	public static final ImageIcon caseVideDeplacement = new ImageIcon("ressources/images/cases/deplacement/CaseVide.png");
+	public static final ImageIcon casePersoDeplacement = new ImageIcon("ressources/images/cases/deplacement/Bryan2.png");
+	public static final ImageIcon caseArbre = new ImageIcon("ressources/images/cases/deplacement/CaseArbre.png");
+	public static final ImageIcon maisonHG = new ImageIcon("ressources/images/cases/deplacement/MaisonHG.png");
+	public static final ImageIcon maisonHD = new ImageIcon("ressources/images/cases/deplacement/MaisonHD.png");
+	public static final ImageIcon maisonBG = new ImageIcon("ressources/images/cases/deplacement/MaisonBG.png");
+	public static final ImageIcon maisonBD = new ImageIcon("ressources/images/cases/deplacement/MaisonBD.png");
+	public static final ImageIcon coffre = new ImageIcon("ressources/images/cases/deplacement/coffre.png");
+	public static final ImageIcon coffreOuvert = new ImageIcon("ressources/images/cases/deplacement/coffreOuvert.png");
+	public static final ImageIcon barriere = new ImageIcon("ressources/images/cases/deplacement/barriere.png");
+	public static final ImageIcon zombie_depl = new ImageIcon("ressources/images/cases/deplacement/zombie.png");
+	
+	// Mode combat
+	public static final String MAP_COMBAT = "ressources/mapsPattern/mapCombat.txt";
+	public static final ImageIcon casePersoAttaque = new ImageIcon("ressources/images/cases/attaque/Bryan2.png");
+	public static final ImageIcon caseVideCombat = new ImageIcon("ressources/images/cases/attaque/caseVideCombat.png");
+	public static final ImageIcon caseObstacle = new ImageIcon("ressources/images/cases/attaque/caseObstacle.png");
+	public static final ImageIcon zombie_att = new ImageIcon("ressources/images/cases/attaque/zombie.png");
+
 }
--- src/FenetreDeJeu.java
+++ src/FenetreDeJeu.java
@@ -1,10 +1,27 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.event.*;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.InternalFrameListener;
 
 public class FenetreDeJeu extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -12,37 +29,31 @@ public class FenetreDeJeu extends JFrame {
 	private JLabel[][] labels;
 	private BufferedReader reader;
 	private int[][] pattern;
-	private MoveListener ecouteurDepl = new MoveListener();
+	private MoveListenerModeJeu listenerJeu = new MoveListenerModeJeu();
+	private MoveListenerModeCombat listenerCombat = new MoveListenerModeCombat();
 	private Dimension coord;
 	private JPanel panelMap, panelCarac;
 	private JInternalFrame frameInventaire;
-	private Personnage perso;
+	private Personnage perso = new Personnage("Personnage_Test");
 
 	public FenetreDeJeu() {
 		super("ZergoLand");
 		setLayout(new BorderLayout());
-		initListener();
-		initIventaire();
-		initReader();
-		initMap();
 		initCarac();
+		initIventaire();
+		initMapJeu();
 		initFenetre();
 	}
-	private void initIventaire(){
+	private void initIventaire() {
 		frameInventaire = new JInternalFrame("Inventaire", true, true, true);
 		frameInventaire.setSize(500, 250);
 		frameInventaire.setLocation(772, 420);
 		frameInventaire.addInternalFrameListener(new InternalFrameListener() {
-			public void internalFrameOpened(InternalFrameEvent arg0) {
-			}
-			public void internalFrameIconified(InternalFrameEvent arg0) {
-			}
-			public void internalFrameDeiconified(InternalFrameEvent arg0) {
-			}
-			public void internalFrameDeactivated(InternalFrameEvent arg0) {
-			}
-			public void internalFrameClosing(InternalFrameEvent arg0) {
-			}
+			public void internalFrameOpened(InternalFrameEvent arg0) {}
+			public void internalFrameIconified(InternalFrameEvent arg0) {}
+			public void internalFrameDeiconified(InternalFrameEvent arg0) {}
+			public void internalFrameDeactivated(InternalFrameEvent arg0) {}
+			public void internalFrameClosing(InternalFrameEvent arg0) {}
 			public void internalFrameClosed(InternalFrameEvent arg0) {
 				setFocusable(true);
 				requestFocusInWindow();
@@ -61,26 +72,26 @@ public class FenetreDeJeu extends JFrame {
 		JProgressBar mana = new JProgressBar(0, perso.getManaMax());
 		JLabel tmp;
 		JButton inventaire = new JButton("Inventaire");
-		
+
 		// Panel Carac
 		panelCarac = new JPanel();
 		panelCarac.setLayout(new BoxLayout(panelCarac, BoxLayout.Y_AXIS));
 		panelCarac.setBorder(BorderFactory.createTitledBorder(null, s, TitledBorder.CENTER, TitledBorder.TOP, f, Color.BLACK));
 		panelCarac.add(new JLabel(" "));
-		
+
 		// Nom
 		tmp = new JLabel(perso.getNom());
 		tmp.setForeground(Color.RED);
 		tmp.setFont(new Font("Arial", Font.ITALIC, 15));
 		panelCarac.add(tmp);
 		panelCarac.add(new JLabel(" "));
-		
+
 		// Lvl
 		tmp = new JLabel("Lvl " + perso.getLvl());
 		tmp.setFont(new Font("Arial", Font.ITALIC, 22));
 		panelCarac.add(tmp);
 		panelCarac.add(new JLabel(" "));
-		
+
 		// Vita
 		vita.setValue(perso.getHp());
 		tmp = new JLabel("Vitalité");
@@ -88,20 +99,20 @@ public class FenetreDeJeu extends JFrame {
 		panelCarac.add(tmp);
 		panelCarac.add(vita);
 		panelCarac.add(new JLabel(" "));
-		
+
 		// Mana
 		tmp = new JLabel("Mana");
 		tmp.setFont(f);
 		panelCarac.add(tmp);
 		panelCarac.add(mana);
 		panelCarac.add(new JLabel(" "));
-		
+
 		// Or
-		tmp = new JLabel("Or:    "+ perso.getOr()+ " pièces");
+		tmp = new JLabel("Or:    " + perso.getOr() + " pièces");
 		tmp.setFont(f);
 		panelCarac.add(tmp);
 		panelCarac.add(new JLabel(" "));
-		
+
 		// Bouton inventaire
 		inventaire.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -109,14 +120,19 @@ public class FenetreDeJeu extends JFrame {
 			}
 		});
 		panelCarac.add(inventaire);
-		
+
 		// Final add
 		add(panelCarac);
 	}
-	private void initListener() {
+	private void initListenerJeu() {
 		setFocusable(true);
 		requestFocusInWindow();
-		addKeyListener(ecouteurDepl);
+		addKeyListener(listenerJeu);
+	}
+	private void initListenerCombat() {
+		setFocusable(true);
+		requestFocusInWindow();
+		addKeyListener(listenerCombat);
 	}
 	private void initFenetre() {
 		setSize(1280, 700);
@@ -126,12 +142,12 @@ public class FenetreDeJeu extends JFrame {
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	private void initReader() {
+	private void initReader(String map) {
 		String[] patternTmp = null;
 		int cpt = 0;
 		try {
 			pattern = new int[Constante.CASES_X][Constante.CASES_Y];
-			reader = new BufferedReader(new FileReader(Constante.MAP));
+			reader = new BufferedReader(new FileReader(map));
 			while (reader.ready()) {
 				patternTmp = reader.readLine().split(",");
 				for (int i = 0; i < patternTmp.length; i++)
@@ -141,21 +157,22 @@ public class FenetreDeJeu extends JFrame {
 		}
 		catch (IOException e) {}
 	}
-	private void initMap() {
+	private void initMapJeu() {
 		panelMap = new JPanel();
 		panelMap.setLayout(new GridLayout(Constante.CASES_X, Constante.CASES_Y));
 		map = new Case[Constante.CASES_X][Constante.CASES_Y];
 		labels = new JLabel[Constante.CASES_X][Constante.CASES_Y];
 
+		initListenerJeu();
+		initReader(Constante.MAP_DEPLACEMENT);
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
 				if (i == 3 && j == 8) {
-					perso = new Personnage("Personnage_Test");
-					map[i][j] = new Case(Constante.casePerso);
+					map[i][j] = new Case(Constante.casePersoDeplacement);
 					coord = new Dimension(i, j);
 				}
 				else if (pattern[i][j] == 0)
-					map[i][j] = new Case(Constante.caseVide);
+					map[i][j] = new Case(Constante.caseVideDeplacement);
 				else if (pattern[i][j] == 1)
 					map[i][j] = new Case(Constante.caseArbre);
 				else if (pattern[i][j] == 2)
@@ -172,6 +189,8 @@ public class FenetreDeJeu extends JFrame {
 					map[i][j] = new Case(Constante.barriere);
 				else if (pattern[i][j] == 8)
 					map[i][j] = new Case(Constante.coffreOuvert);
+				else if (pattern[i][j] == 9)
+					map[i][j] = new Case(Constante.zombie_depl);
 
 				labels[i][j] = new JLabel();
 				labels[i][j].setIcon(map[i][j].getSkin());
@@ -181,62 +200,64 @@ public class FenetreDeJeu extends JFrame {
 		}
 	}
 
-	private class MoveListener implements KeyListener {
+	private class MoveListenerModeJeu implements KeyListener {
 		public void keyPressed(KeyEvent e) {
 			int x = coord.width;
 			int y = coord.height;
 
 			try {
-				if (e.getKeyCode() == KeyEvent.VK_Z || e.getKeyCode() == KeyEvent.VK_UP) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
+					initMapCombat();
+				else if (e.getKeyCode() == KeyEvent.VK_Z || e.getKeyCode() == KeyEvent.VK_UP) {
 					// Case apres
 					if (pattern[x - 1][y] == 0) {
-						map[x - 1][y].setSkin(Constante.casePerso); // deplacement du perso
+						map[x - 1][y].setSkin(Constante.casePersoDeplacement); // deplacement du perso
 						coord = new Dimension(x - 1, y); // update coord
 
 						// Ajout au label
 						labels[x - 1][y].setIcon(map[x - 1][y].getSkin());
 
 						// Case avant
-						map[x][y].setSkin(Constante.caseVide);
+						map[x][y].setSkin(Constante.caseVideDeplacement);
 					}
 				}
 				else if (e.getKeyCode() == KeyEvent.VK_Q || e.getKeyCode() == KeyEvent.VK_LEFT) {
 					// Case apres
 					if (pattern[x][y - 1] == 0) {
-						map[x][y - 1].setSkin(Constante.casePerso); // deplacement du perso
+						map[x][y - 1].setSkin(Constante.casePersoDeplacement); // deplacement du perso
 						coord = new Dimension(x, y - 1); // update coord
 
 						// Ajout au label
 						labels[x][y - 1].setIcon(map[x][y - 1].getSkin());
 
 						// Case avant
-						map[x][y].setSkin(Constante.caseVide);
+						map[x][y].setSkin(Constante.caseVideDeplacement);
 					}
 				}
 				else if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
 					// Case apres
 					if (pattern[x + 1][y] == 0) {
-						map[x + 1][y].setSkin(Constante.casePerso); // deplacement du perso
+						map[x + 1][y].setSkin(Constante.casePersoDeplacement); // deplacement du perso
 						coord = new Dimension(x + 1, y); // update coord
 
 						// Ajout au label
 						labels[x + 1][y].setIcon(map[x + 1][y].getSkin());
 
 						// Case avant
-						map[x][y].setSkin(Constante.caseVide);
+						map[x][y].setSkin(Constante.caseVideDeplacement);
 					}
 				}
 				else if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
 					// Case apres
 					if (pattern[x][y + 1] == 0) {
-						map[x][y + 1].setSkin(Constante.casePerso); // deplacement du perso
+						map[x][y + 1].setSkin(Constante.casePersoDeplacement); // deplacement du perso
 						coord = new Dimension(x, y + 1); // update coord
 
 						// Ajout au label
 						labels[x][y + 1].setIcon(map[x][y + 1].getSkin());
 
 						// Case avant
-						map[x][y].setSkin(Constante.caseVide);
+						map[x][y].setSkin(Constante.caseVideDeplacement);
 					}
 				}
 				// Ajout au label
@@ -246,5 +267,100 @@ public class FenetreDeJeu extends JFrame {
 		}
 		public void keyReleased(KeyEvent e) {}
 		public void keyTyped(KeyEvent e) {}
+	}
+
+	private class MoveListenerModeCombat implements KeyListener {
+		public void keyPressed(KeyEvent e) {
+			int x = coord.width;
+			int y = coord.height;
+
+			try {
+				if (e.getKeyCode() == KeyEvent.VK_Z || e.getKeyCode() == KeyEvent.VK_UP) {
+					// Case apres
+					if (pattern[x - 1][y] == 0) {
+						map[x - 1][y].setSkin(Constante.casePersoAttaque); // deplacement du perso
+						coord = new Dimension(x - 1, y); // update coord
+
+						// Ajout au label
+						labels[x - 1][y].setIcon(map[x - 1][y].getSkin());
+
+						// Case avant
+						map[x][y].setSkin(Constante.caseVideCombat);
+					}
+				}
+				else if (e.getKeyCode() == KeyEvent.VK_Q || e.getKeyCode() == KeyEvent.VK_LEFT) {
+					// Case apres
+					if (pattern[x][y - 1] == 0) {
+						map[x][y - 1].setSkin(Constante.casePersoAttaque); // deplacement du perso
+						coord = new Dimension(x, y - 1); // update coord
+
+						// Ajout au label
+						labels[x][y - 1].setIcon(map[x][y - 1].getSkin());
+
+						// Case avant
+						map[x][y].setSkin(Constante.caseVideCombat);
+					}
+				}
+				else if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
+					// Case apres
+					if (pattern[x + 1][y] == 0) {
+						map[x + 1][y].setSkin(Constante.casePersoAttaque); // deplacement du perso
+						coord = new Dimension(x + 1, y); // update coord
+
+						// Ajout au label
+						labels[x + 1][y].setIcon(map[x + 1][y].getSkin());
+
+						// Case avant
+						map[x][y].setSkin(Constante.caseVideCombat);
+					}
+				}
+				else if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					// Case apres
+					if (pattern[x][y + 1] == 0) {
+						map[x][y + 1].setSkin(Constante.casePersoAttaque); // deplacement du perso
+						coord = new Dimension(x, y + 1); // update coord
+
+						// Ajout au label
+						labels[x][y + 1].setIcon(map[x][y + 1].getSkin());
+
+						// Case avant
+						map[x][y].setSkin(Constante.caseVideCombat);
+					}
+				}
+				// Ajout au label
+				labels[x][y].setIcon(map[x][y].getSkin());
+			}
+			catch (java.lang.ArrayIndexOutOfBoundsException ex) {}
+		}
+		public void keyReleased(KeyEvent e) {}
+		public void keyTyped(KeyEvent e) {}
+	}
+
+	public void initMapCombat() {
+		panelMap = new JPanel();
+		panelMap.setLayout(new GridLayout(Constante.CASES_X, Constante.CASES_Y));
+		map = new Case[Constante.CASES_X][Constante.CASES_Y];
+		labels = new JLabel[Constante.CASES_X][Constante.CASES_Y];
+
+		getContentPane().removeAll();
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if (i == 3 && j == 8) {
+					map[i][j] = new Case(Constante.casePersoAttaque);
+					coord = new Dimension(i, j);
+				}
+				if (pattern[i][j] == 9)
+					map[i][j] = new Case(Constante.zombie_att);
+				else if (pattern[i][j] == 0)
+					map[i][j] = new Case(Constante.caseVideCombat);
+				else
+					map[i][j] = new Case(Constante.caseObstacle);
+
+				labels[i][j] = new JLabel();
+				labels[i][j].setIcon(map[i][j].getSkin());
+				panelMap.add(labels[i][j]);
+				add(panelMap, BorderLayout.WEST);
+			}
+		}
 	}
 }
